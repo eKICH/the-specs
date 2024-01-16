8b3cd381d7758883586d768eebae1cb0a38b7807
Chris <christophermutuma@gmail.com>
2024-01-17 00:41:14 +0300

    feature-product-details

--- src/app/components/products/product-details/product-details.component.ts
+++ src/app/components/products/product-details/product-details.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../../../models/product.interface';
+import { ProductService } from '../../../services/product.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit, OnDestroy{
 
+  productService: ProductService = inject(ProductService);
+  activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+  productId!: number;
+  selectedProduct!: Product | undefined;
+  prodObs!: any;
+
+  ngOnInit(){
+    // this.productId = Number(this.activeRoute.snapshot.paramMap.get('id'));
+    this.prodObs = this.activeRoute.paramMap.subscribe((id) => {
+      this.productId = Number(id.get('id'));
+      this.selectedProduct = this.productService.products.find((prod) => prod.id === this.productId);
+    });
+  }
+
+  ngOnDestroy() {
+    this.prodObs.unsubscribe();
+  }
 }
---
+++ src/models/product.interface.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    id: number,
+    model: string,
+    brand: string,
+    color: string,
+    category: string,
+    imgUrl: string,
+    inches: string,
+    desc: string,
+    features: string[],
+    specs: string[]
+}
