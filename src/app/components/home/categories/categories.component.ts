0a294e5e52afde1e09d58f432bc846cf28ae6016
Chris <christophermutuma@gmail.com>
2024-01-23 15:33:11 +0300

    build-fixed-img-url-paths

--- src/app/components/home/categories/categories.component.ts
+++ src/app/components/home/categories/categories.component.ts
@@ -11,9 +11,9 @@ export class CategoriesComponent {
 
   categories = [
     {imgUrl: 'assets/images/Tvs.png', title: "Television"},
-    {imgUrl: '../../../../assets/images/desktop.png', title: "Desktop"},
-    {imgUrl: '../../../../assets/images/laptop.png', title: "Laptop"},
-    {imgUrl: '../../../../assets/images/phones&tablets.png', title: "Phones"}
+    {imgUrl: 'assets/images/desktop.png', title: "Desktop"},
+    {imgUrl: 'assets/images/laptop.png', title: "Laptop"},
+    {imgUrl: 'assets/images/phones&tablets.png', title: "Phones"}
   ];
 
   onCategoryClick(value: string){
