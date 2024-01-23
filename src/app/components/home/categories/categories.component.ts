5f6f248fc5d269f96a6ec876d624e6e151cbe4b2
Chris <christophermutuma@gmail.com>
2024-01-23 15:19:30 +0300

    updating img url

--- src/app/components/home/categories/categories.component.ts
+++ src/app/components/home/categories/categories.component.ts
@@ -10,7 +10,7 @@ export class CategoriesComponent {
   route: Router = inject(Router);
 
   categories = [
-    {imgUrl: '../../../../assets/images/Tvs.png', title: "Television"},
+    {imgUrl: 'assets/images/Tvs.png', title: "Television"},
     {imgUrl: '../../../../assets/images/desktop.png', title: "Desktop"},
     {imgUrl: '../../../../assets/images/laptop.png', title: "Laptop"},
     {imgUrl: '../../../../assets/images/phones&tablets.png', title: "Phones"}
